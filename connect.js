fbf599f970cefe798ce2f44c0743829a387e0241
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-03-27 19:31:54 -0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<username>:<pasword>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dellesheoniadixon2:<password>@ddixonmdb.05vfedo.mongodb.net/?retryWrites=true&w=majority
+";
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('ddixonmdb');
+    const parts = database.collection('class_examplemongodb');
 
     // Query for a part that has partID '12345'
     const query = { partID: '12345' };
